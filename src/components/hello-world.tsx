73fa718b3a8c9cc39cdb95463cb6d4c84a9c596f
Jade Hernandez <jade.hernandez88@gmail.com>
2025-04-21 16:36:10 +0200

    Add social media icons: implement Facebook, Github, Instagram, Youtube, and X icons

--- src/components/hello-world.tsx
+++ src/components/hello-world.tsx
@@ -1,9 +1,13 @@
 "use client";
 
+import YoutubeIcon from "./icons/youtube-icon";
+
+
 export default function HelloWorld() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <h1 className="text-4xl font-bold">Hello World</h1>
+      <YoutubeIcon />
     </div>
   );
 }
